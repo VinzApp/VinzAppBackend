a5172642e4d0ad45000d60ae7c37e3df8ee6005d
gabriel <gabrielgoller123@gmail.com>
2020-09-22 21:58:47 +0200

    ✨️ Added events expiration

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,7 @@ export default {
     external: [
         'express',
         'sqlite3',
-        'swagger-ui-express'
+        'swagger-ui-express',
+        'tslib'
     ]
 };
--- src/db/events.ts
+++ src/db/events.ts
@@ -1,5 +1,6 @@
 import sqlite3 from 'sqlite3';
 
+type promiseFun = (n: void) => void;
 type CallbackFunc = (s: string) => void;
 
 export default class EventsDatabase {
@@ -10,20 +11,47 @@ export default class EventsDatabase {
     }
 
     getEvents(callback: CallbackFunc): void {
-        this.db.all(`SELECT * FROM Events`, (err: string, res: string) => {
-            if(err) {
-                console.log('Error: ' + err);
-            }
-            callback(res);
-        });
+        // Remove all the expired events, then fetch and return all remaining
+        this.removeExpiredEvents()
+            .then(() => {
+                this.db.all(`SELECT * FROM Events`, (err: string, res: string) => {
+                    if(err) {
+                        console.log('Error: ' + err);
+                    }
+                    callback(res);
+                });
+            })
+            .catch(() => {
+                console.log('An error occured, look above output for more info');
+            });
+            
     }
 
     createEvent(expirationDate: string, text: string): void {
-        this.db.run(`INSERT INTO Events VALUES ('${expirationDate}', '${text}');`, (err: string) => {
+        // Create unix time out of passed date
+        const dateParts: string[] = expirationDate.split('.');
+        const date = new Date(parseInt(dateParts[2]), parseInt(dateParts[1])-1, parseInt(dateParts[0])+1);
+        // Insert data in database
+        this.db.run(`INSERT INTO Events VALUES ('${expirationDate}', ${date.getTime() / 1000}, '${text}');`, (err: string) => {
             if(err){
                 console.log('Error: ' + err);
             }
         });
     }
 
+    async removeExpiredEvents(): Promise<void> {
+        return new Promise<void>((resolve: promiseFun, reject: promiseFun) => {
+            const date: Date = new Date();
+            // Convert unixtime from milliseconds to seconds
+            const unixTime = Math.floor(date.getTime() / 1000);
+            // Delete all expired dates 
+            this.db.run(`DELETE FROM Events WHERE expiration_unix<${unixTime}`, (err: string) => {
+                if(err) {
+                    console.log('Error: ' + err);
+                    reject();
+                }
+                resolve();
+            });
+        });
+    }
 }
